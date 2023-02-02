1662a5950882fe5468fa77e3cf6fdd2f4011829a
waito3209 <60461394+waito3209@users.noreply.github.com>
2023-02-02 17:04:12 +0800

    create link list file

---
+++ LinkList.c
@@ -0,0 +1,5 @@
+//
+// Created by User on 2/2/2023.
+//
+
+#include "LinkList.h"
---
+++ LinkList.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 2/2/2023.
+//
+
+#ifndef REV_LINKLIST_H
+#define REV_LINKLIST_H
+
+#endif //REV_LINKLIST_H
