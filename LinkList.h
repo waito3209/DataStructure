66073dbc43d7141f61c432ed30a7d55c3609ae2f
waito3209 <60461394+waito3209@users.noreply.github.com>
2023-02-04 14:56:33 +0800

    defined linklist .h file

--- LinkList.h
+++ LinkList.h
@@ -4,5 +4,14 @@
 
 #ifndef REV_LINKLIST_H
 #define REV_LINKLIST_H
+typedef struct Linklistnode Linklistnode;
+typedef Linklistnode * Linklistnodep ;
+typedef struct Linklist Linklist;
+typedef Linklist * LinkListp;
+void LinklistAdd(LinkListp l,int index, void * data );
+void LinklistRemove(LinkListp l,int index, void * data);
+void * LinklistGet(LinkListp l,int index, void * data);
+int LinklistFind(LinkListp l,int index, void * data);
+
 
 #endif //REV_LINKLIST_H
